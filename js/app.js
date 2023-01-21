5f9b35e74d6ce0a967f428d120ee782f9afe3d7e
alexis0916 <brayhan.alexis@hotmail.com>
2023-01-21 14:11:00 -0500

    entrega 2 maestro pokemon

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
-let eleccion = parseInt( prompt("Digita el numero correspondiente a tu eleccion de tipo de pokemon: \n1: Agua\n2: Planta\n3: Fuego"))
+let eleccion = parseInt(
+  prompt(
+    "Digita el numero correspondiente a tu eleccion de tipo de pokemon: \n1: Agua\n2: Planta\n3: Fuego"
+  )
+);
 let pokemon1 = "Squirtle";
 let pokemon2 = "Bulbasaur";
 let pokemon3 = "Chanmander";
@@ -14,48 +18,53 @@ let peleas;
   console.log("no hay pokemon disponible de ese tipo");
 } */
 
+function escribirPantalla(poke, clase, tipomsg, index) {
+  if (tipomsg === 1) {
+    document.write(
+      `El pokemon que seleccionaste de tipo ${clase} es: ${poke} <br>`
+    );
+  }
+  if (tipomsg === 2) {
+    document.write(
+      " Tu " + poke + " ha ganado la batalla numero: " + index + "<br>"
+    );
+  }
+}
 
-function escribirPantalla(poke,clase,tipomsg,index) {
-if (tipomsg === 1) {
-    document.write(`El pokemon que seleccionaste de tipo ${clase} es: ${poke} <br>`);    
-}
-if (tipomsg === 2) {
-    document.write(" Tu " + poke + " ha ganado la batalla numero: " + index + "<br>");
-}
-}
-
-function alerta(escogido,clase) {
-    alert(`El pokemon que seleccionaste de tipo ${clase} es: ${escogido}`);
+function alerta(escogido, clase) {
+  alert(`El pokemon que seleccionaste de tipo ${clase} es: ${escogido}`);
 }
 
 function pedirBatallas(poke) {
-    let battle = prompt( `¿Cuantas Batallas quieres que tu ${poke} luche ?`);   
-   return battle
+  let battle = parseInt(
+    prompt(`¿Cuantas Batallas quieres que tu ${poke} luche ?`)
+  );
+  return battle;
 }
 
 switch (eleccion) {
   case 1:
-    alerta(pokemon1,"agua");
-    escribirPantalla(pokemon1,"agua",1);
+    alerta(pokemon1, "agua");
+    escribirPantalla(pokemon1, "agua", 1);
     peleas = pedirBatallas(pokemon1);
     for (let index = 1; index <= peleas; index++) {
-        escribirPantalla(pokemon1,"agua",2, index)
-    };
+      escribirPantalla(pokemon1, "agua", 2, index);
+    }
     break;
   case 2:
-    alerta(pokemon2,"planta");
-    escribirPantalla(pokemon2, "planta",1);
+    alerta(pokemon2, "planta");
+    escribirPantalla(pokemon2, "planta", 1);
     peleas = pedirBatallas(pokemon2);
     for (let index = 1; index <= peleas; index++) {
-        escribirPantalla(pokemon2,"planta",2, index)
-    };
+      escribirPantalla(pokemon2, "planta", 2, index);
+    }
     break;
   case 3:
-    alerta(pokemon3,"fuego");
-    escribirPantalla(pokemon3, "fuego",1)
-    peleas = pedirBatallas(pokemon3)
+    alerta(pokemon3, "fuego");
+    escribirPantalla(pokemon3, "fuego", 1);
+    peleas = pedirBatallas(pokemon3);
     for (let index = 1; index <= peleas; index++) {
-        escribirPantalla(pokemon3,"fuego",2, index)
+      escribirPantalla(pokemon3, "fuego", 2, index);
     }
     break;
 
