1e86cbfd9c4c2b7252f9bff1971a3a3f7eb5c010
alexis0916 <brayhan.alexis@hotmail.com>
2023-01-22 23:25:22 -0500

    entrega 2 se agregan math random

--- js/app.js
+++ js/app.js
@@ -8,7 +8,8 @@ let pokemon2 = "Bulbasaur";
 let pokemon3 = "Chanmander";
 let peleas;
 
-/* if (eleccion === "agua") {
+
+ if (eleccion === "agua") {
   console.log("el pokemon de agua es: " + pokemon1);
 } else if (eleccion === "planta") {
   console.log("el pokemon de planta es: " + pokemon2);
@@ -16,9 +17,9 @@ let peleas;
   console.log("el pokemon de fuego es: " + pokemon3);
 } else {
   console.log("no hay pokemon disponible de ese tipo");
-} */
+} 
 
-function escribirPantalla(poke, clase, tipomsg, index) {
+function escribirPantalla(poke, clase, tipomsg, index,resultBatalla) {
   if (tipomsg === 1) {
     document.write(
       `El pokemon que seleccionaste de tipo ${clase} es: ${poke} <br>`
@@ -26,9 +27,9 @@ function escribirPantalla(poke, clase, tipomsg, index) {
   }
   if (tipomsg === 2) {
     document.write(
-      " Tu " + poke + " ha ganado la batalla numero: " + index + "<br>"
-    );
+      " Tu " + poke + " ha " + resultBatalla+ " la batalla numero: " + index + "<br>");
   }
+
 }
 
 function alerta(escogido, clase) {
@@ -41,6 +42,12 @@ function pedirBatallas(poke) {
   );
   return battle;
 }
+   
+function aleatorio() {
+  const result = Math.floor ( Math.random() *2 ) +1
+   return result
+ }
+
 
 switch (eleccion) {
   case 1:
@@ -48,7 +55,19 @@ switch (eleccion) {
     escribirPantalla(pokemon1, "agua", 1);
     peleas = pedirBatallas(pokemon1);
     for (let index = 1; index <= peleas; index++) {
-      escribirPantalla(pokemon1, "agua", 2, index);
+      const result = aleatorio()
+      switch (result) {
+        case 1:
+          console.log(result)
+          escribirPantalla(pokemon1, "agua", 2, index," ganado");
+          break;
+          case 2:
+            console.log(result)
+            escribirPantalla(pokemon1, "agua", 2, index," perdido");
+            break;
+      
+      }
+
     }
     break;
   case 2:
@@ -56,7 +75,18 @@ switch (eleccion) {
     escribirPantalla(pokemon2, "planta", 1);
     peleas = pedirBatallas(pokemon2);
     for (let index = 1; index <= peleas; index++) {
-      escribirPantalla(pokemon2, "planta", 2, index);
+      const result = aleatorio()
+      switch (result) {
+        case 1:
+          console.log(result)
+          escribirPantalla(pokemon1, "agua", 2, index," ganado");
+          break;
+          case 2:
+            console.log(result)
+            escribirPantalla(pokemon1, "agua", 2, index," perdido");
+            break;
+      
+      }
     }
     break;
   case 3:
@@ -64,11 +94,27 @@ switch (eleccion) {
     escribirPantalla(pokemon3, "fuego", 1);
     peleas = pedirBatallas(pokemon3);
     for (let index = 1; index <= peleas; index++) {
-      escribirPantalla(pokemon3, "fuego", 2, index);
+      const result = aleatorio()
+      switch (result) {
+        case 1:
+          console.log(result)
+          escribirPantalla(pokemon1, "agua", 2, index," ganado");
+          break;
+          case 2:
+            console.log(result)
+            escribirPantalla(pokemon1, "agua", 2, index," perdido");
+            break;
+      
+      }
     }
     break;
 
   default:
     document.write("no hay pokemon disponible de ese tipo");
     break;
-}
+
+}  
+
+
+
+
