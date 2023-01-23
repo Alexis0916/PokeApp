7684468466284df7a9fbcf92958b5921647befc5
alexis0916 <brayhan.alexis@hotmail.com>
2023-01-23 00:17:39 -0500

    entrega 2 se agregan arrays y objetos

--- js/app.js
+++ js/app.js
@@ -1,33 +1,52 @@
+
+
 let eleccion = parseInt(
   prompt(
     "Digita el numero correspondiente a tu eleccion de tipo de pokemon: \n1: Agua\n2: Planta\n3: Fuego"
   )
 );
+
 let pokemon1 = "Squirtle";
 let pokemon2 = "Bulbasaur";
 let pokemon3 = "Chanmander";
 let peleas;
+let powers
+const firePowers = ["Bola De Fuego", "Lanza LLamas", "Calcinacion", "Puño de fuego" ]
+const waterPowers = ["Pistola de agua", "Hidro Bomba", "Hidro Pulso", "Surf"]
+const plantPowers = ["Rayo solar", "Latigo Cepa", "Hojas Navaja", "Veneno Paralizador"]
+
+//Objeto
+const pokemon = {
+  nombre : ''
+}
+
+//Asingar propiedad al key nombre
+pokemon.nombre = prompt("Ingresa el nombre de tu pokemon")
 
 
- if (eleccion === "agua") {
-  console.log("el pokemon de agua es: " + pokemon1);
-} else if (eleccion === "planta") {
-  console.log("el pokemon de planta es: " + pokemon2);
-} else if (eleccion === "fuego") {
-  console.log("el pokemon de fuego es: " + pokemon3);
-} else {
-  console.log("no hay pokemon disponible de ese tipo");
-} 
 
 function escribirPantalla(poke, clase, tipomsg, index,resultBatalla) {
   if (tipomsg === 1) {
-    document.write(
-      `El pokemon que seleccionaste de tipo ${clase} es: ${poke} <br>`
-    );
+    document.write(`El pokemon que seleccionaste de tipo ${clase} es: ${poke} <br>`);
+
+    document.write(`El nombre de tu pokemon es: ${ pokemon.nombre} <br>` )
+
+      if (eleccion === 1) {
+        powers = randomElements(waterPowers,2)
+      } else if (eleccion === 2){
+        powers = randomElements(plantPowers,2)
+      } else if (eleccion === 3){ 
+        powers = randomElements(firePowers,2)
+      }
+
+  document.write("Los poderes asignados a tu pokemon son: <br>")
+  for (let index = 0; index < powers.length; index++) {
+    //Index = pocision del array
+    document.write(`${powers[index]} <br>`)
+    }
   }
   if (tipomsg === 2) {
-    document.write(
-      " Tu " + poke + " ha " + resultBatalla+ " la batalla numero: " + index + "<br>");
+    document.write( " Tu " + poke + " ha " + resultBatalla+ " la batalla numero: " + index + "<br>");
   }
 
 }
@@ -37,9 +56,7 @@ function alerta(escogido, clase) {
 }
 
 function pedirBatallas(poke) {
-  let battle = parseInt(
-    prompt(`¿Cuantas Batallas quieres que tu ${poke} luche ?`)
-  );
+  let battle = parseInt(prompt(`¿Cuantas Batallas quieres que tu ${poke} luche ?`));
   return battle;
 }
    
@@ -79,11 +96,11 @@ switch (eleccion) {
       switch (result) {
         case 1:
           console.log(result)
-          escribirPantalla(pokemon1, "agua", 2, index," ganado");
+          escribirPantalla(pokemon1, "planta", 2, index," ganado");
           break;
           case 2:
             console.log(result)
-            escribirPantalla(pokemon1, "agua", 2, index," perdido");
+            escribirPantalla(pokemon1, "planta", 2, index," perdido");
             break;
       
       }
@@ -98,11 +115,11 @@ switch (eleccion) {
       switch (result) {
         case 1:
           console.log(result)
-          escribirPantalla(pokemon1, "agua", 2, index," ganado");
+          escribirPantalla(pokemon1, "fuego", 2, index," ganado");
           break;
           case 2:
             console.log(result)
-            escribirPantalla(pokemon1, "agua", 2, index," perdido");
+            escribirPantalla(pokemon1, "fuego", 2, index," perdido");
             break;
       
       }
@@ -115,6 +132,22 @@ switch (eleccion) {
 
 }  
 
+
+
+//******************************* */
+//Funcion para extraer un aleatorio
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+function randomElements(array, quantity) {
+return shuffleArray([...array]).slice(0, quantity);
+}
+
+//******************************* */
 
 
 
